e0d350f639606a92c2b061045daf5a787730dcb6
fasihh <fhk.tcs@gmail.com>
2025-03-22 00:08:33 +0500

    added client and server impl

--- src/client_main.cpp
+++ src/client_main.cpp
@@ -1,14 +1,60 @@
 #include <iostream>
-#include "../include/socket.hpp"
+#include <string>
+#include <thread>
+#include <atomic>
+#include <functional>
+#include <socket.hpp>
 
-using namespace std;
+std::atomic<bool> error_status(false);
+
+void receive_messages(os_sock::Socket client_socket) {
+    try {
+        while (true) {
+            std::string response = client_socket.recv();
+            if (response.empty())
+                continue;
+            std::cout << ">> " << response << "\n";
+        }
+    } catch (std::exception& err) {
+        std::cout << "receive_messages | " << err.what() << std::endl;
+        error_status = true;
+    }
+}
 
 int main() {
 	os_sock::Socket client(AF_INET, SOCK_STREAM);
+    client.connect("127.0.0.1", 8080);
 
-	client.connect("127.0.0.1", 8080);
+    // std::thread read_thread(std::bind(receive_messages, client));
+    // read_thread.detach();
 
-	cout << client.send("Hello Server!") << endl;
+    std::string buffer;
+    try {
+        while (!error_status) {
+            std::cout << "<< ";
+            std::getline(std::cin, buffer);
+            if (buffer.size() > BUFSIZ)
+                buffer = buffer.substr(BUFSIZ);
+    
+            if (buffer == ":quit")
+                break;
+
+            std::string response = client.recv();
+            if (response.empty())
+                continue;
+            std::cout << ">> " << response << "\n";
+            
+            client.send(buffer);
+        }
+    } catch (std::exception& err) {
+        std::cout << "main | " << err.what() << std::endl;
+        client.close();
+        exit(EXIT_FAILURE);
+    } catch (...) {
+        std::cout << "main | unexpected error" << std::endl;
+        client.close();
+        exit(EXIT_FAILURE);
+    }
 
 	return 0;
 }
--- src/server_main.cpp
+++ src/server_main.cpp
@@ -1,7 +1,62 @@
-#include "../include/socket.hpp"
 #include <iostream>
+#include <utility>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <thread>
+#include <mutex>
+#include <socket.hpp>
 
-using namespace std;
+std::vector<std::pair<os_sock::Socket, os_sock::addr_p_t>> clients;
+std::mutex client_mutex;
+
+std::ostream& operator<<(std::ostream& out, const os_sock::addr_p_t& addr_p) {
+    out << addr_p.first << ":" << addr_p.second;
+    return out;
+}
+
+void remove_client(os_sock::Socket& client_socket) {
+    std::lock_guard<std::mutex> lock(client_mutex);
+    std::remove_if(
+        clients.begin(),
+        clients.end(),
+        [&](const std::pair<os_sock::Socket, os_sock::addr_p_t>& client) {
+            return client.first == client_socket;
+        }
+    );
+    client_socket.close();
+}
+
+void broadcast(const std::string& message, os_sock::Socket& sender) {
+    for (auto& [client, address] : clients) {
+        if (client == sender)
+            continue;
+        ssize_t bytes_sent = client.send(message);
+        if (bytes_sent < 0)
+            throw std::runtime_error("broadcast: error sending message");
+    }
+}
+
+void handle_client(os_sock::Socket client_socket, os_sock::addr_p_t client_address) {
+    ssize_t bytes_sent = client_socket.send("Welcome!");
+    if (bytes_sent < 0)
+        throw std::runtime_error("handle_client: error sending message to client");
+    
+    try {
+        while (true) {
+            std::string response = client_socket.recv();
+            if (response.empty())
+                continue;
+            std::cout << client_address << " = " << response << "\n";
+            broadcast(response, client_socket);
+        }
+    } catch (std::exception& err) {
+        std::cout << "handle_client | " << err.what() << std::endl;
+    }
+
+    remove_client(client_socket);
+    std::cout << client_address << " disconnected" << std::endl;
+}
 
 int main() {
     os_sock::Socket server(AF_INET, SOCK_STREAM);
@@ -9,10 +64,26 @@ int main() {
     server.bind("127.0.0.1", 8080);
     server.listen(5);
 
-    while (true) {
-        auto [client_socket, client_address] = server.accept();
-        string response = client_socket.recv();
-        cout << client_address.first << ":" << client_address.second << " = " << response << endl;
+    try {
+        while (true) {
+            auto [client_socket, client_address] = server.accept();
+    
+            std::cout << client_address << " connected\n";
+    
+            std::lock_guard<std::mutex> lock(client_mutex);
+            clients.push_back({client_socket, client_address});
+    
+            std::thread client_thread(std::bind(handle_client, client_socket, client_address));
+            client_thread.detach();
+        }
+    } catch (std::exception& err) {
+        std::cout << "main | " << err.what() << std::endl;
+        server.close();
+        exit(EXIT_FAILURE);
+    } catch (...) {
+        std::cout << "main | unexpected error" << std::endl;
+        server.close();
+        exit(EXIT_FAILURE);
     }
 
     return 0;
