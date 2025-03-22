4cd1bd8e80b798558f5c77a2b1c53d95dca85536
fasihh <fhk.tcs@gmail.com>
2025-03-22 16:02:43 +0500

    slight changes in lock and fixed client removal. no longer detaching thread upon creation

--- src/server_main.cpp
+++ src/server_main.cpp
@@ -5,47 +5,53 @@
 #include <functional>
 #include <thread>
 #include <mutex>
+#include <signal.h>
 #include <socket.hpp>
 
 std::vector<std::pair<os_sock::Socket, os_sock::SocketAddress>> clients;
+std::vector<std::thread> threads;
 std::mutex client_mutex;
 
-void remove_client(os_sock::Socket& client_socket) {
+void remove_client(os_sock::SocketAddress& client_addr) {
     std::lock_guard<std::mutex> lock(client_mutex);
-    std::remove_if(
-        clients.begin(),
-        clients.end(),
-        [&](const std::pair<os_sock::Socket, os_sock::SocketAddress>& client) {
-            return client.first == client_socket;
-        }
+    clients.erase(
+        std::remove_if(
+            clients.begin(),
+            clients.end(),
+            [&](const std::pair<os_sock::Socket, os_sock::SocketAddress>& client) {
+                auto [sock, addr] = client;
+                return addr == client_addr;
+            }
+        ),
+        clients.end()
     );
-    client_socket.close();
 }
 
-void broadcast(const std::string& message, os_sock::Socket& sender) {
+void broadcast(const std::string& message, os_sock::SocketAddress& sender_addr) {
+    std::lock_guard<std::mutex> lock(client_mutex);
     for (auto& [client, address] : clients) {
-        if (client == sender)
+        if (address == sender_addr)
             continue;
         client.send(message);
     }
 }
 
-void handle_client(os_sock::Socket client_socket, os_sock::SocketAddress client_address) {
+void handle_client(os_sock::Socket client_sock, os_sock::SocketAddress client_addr) {
     try {
-        client_socket.send("Welcome!");
+        client_sock.send("Welcome!");
         while (true) {
-            std::string response = client_socket.recv();
+            std::string response = client_sock.recv();
             if (response.empty())
                 continue;
-            std::cout << client_address << " = " << response << "\n";
-            broadcast(response, client_socket);
+            std::cout << client_addr << " = " << response << "\n";
+            broadcast(response, client_addr);
         }
     } catch (std::exception& err) {
         std::cout << "handle_client | " << err.what() << std::endl;
     }
 
-    remove_client(client_socket);
-    std::cout << client_address << " disconnected" << std::endl;
+    remove_client(client_addr);
+    std::cout << client_addr << " disconnected" << std::endl;
 }
 
 int main() {
@@ -59,12 +65,15 @@ int main() {
             auto [client_socket, client_address] = server.accept();
     
             std::cout << client_address << " connected\n";
+
+            {
+                std::lock_guard<std::mutex> lock(client_mutex);
+                clients.push_back({client_socket, client_address});
+            }
     
-            std::lock_guard<std::mutex> lock(client_mutex);
-            clients.push_back({client_socket, client_address});
-    
-            std::thread client_thread(std::bind(handle_client, client_socket, client_address));
-            client_thread.detach();
+            threads.emplace_back(
+                std::thread(handle_client, client_socket, client_address)
+            );
         }
     } catch (std::exception& err) {
         std::cout << "main | " << err.what() << std::endl;
